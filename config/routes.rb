0a4666ac4cadc00bc56a55e2cfbe32aef9cc86fc
diatmpravin <pravinmishra88@gmail.com>
2016-09-06 13:14:40 +0530

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   resources :addresses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'addresses#index'
+  root 'welcome#index'
 end
